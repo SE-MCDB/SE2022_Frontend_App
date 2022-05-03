a0ff1bf0ae52728eb24e633a5984b3c9d1845c72
gmm <2732834341@qq.com>
2022-05-03 15:10:25 +0800

    🎈 perf(ip)

--- api/common.js
+++ api/common.js
@@ -1,6 +1,6 @@
 import axios from '@/config/requestConfig.js';
- export const baseUrl=process.env.NODE_ENV === 'development'?"http://122.9.14.73:8000/api/":"http://api.hfb.xquery.cn/api/";
- export const socketBaseUrl=process.env.NODE_ENV === 'development'?"ws://122.9.14.73:8000/api/":"ws://api.hfb.xquery.cn/api/";
+ export const baseUrl=process.env.NODE_ENV === 'development'?"http://122.9.14.73:8000/api/":"http://122.9.14.73:8000/api/";
+ export const socketBaseUrl=process.env.NODE_ENV === 'development'?"ws://122.9.14.73:8000/api/":"ws://122.9.14.73:8000/api/";
 //可以new多个request来支持多个域名请求
 //点赞功能
 export const  giveLike = async (id) => {
