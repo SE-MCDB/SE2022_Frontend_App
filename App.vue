a4a99adb06f24a957df2c0ccd6bcd3a0207259f9
cpfy <309301477@qq.com>
2022-05-05 19:53:24 +0800

    fix: 解决刷新页面vuex数据丢失问题

--- App.vue
+++ App.vue
@@ -48,6 +48,19 @@
 				}		
 			}
 		},
+		// 解决刷新页面vuex内数据丢失
+		created(){
+			//在页面加载时读取sessionStorage里的状态信息
+			let sessionStorage = window.sessionStorage;
+			if (sessionStorage.getItem("store") ) {
+			  this.$store.replaceState(Object.assign({}, this.$store.state,JSON.parse(sessionStorage.getItem("store"))))
+			}
+
+			//在页面刷新时将vuex里的信息保存到sessionStorage里
+			window.addEventListener("beforeunload",()=>{
+			  sessionStorage.setItem("store",JSON.stringify(this.$store.state))
+			})
+		},
 		onHide: function () {
 			console.log('App Hide')
 		},
