a3ef49a0b86cc009fe6879e8b2cf614e8bdca718
MrXcc0 <2460900079@qq.com>
2022-05-25 15:31:24 +0800

    add：最近评价界面完成

--- api/postEvaluation.js
+++ api/postEvaluation.js
@@ -1,4 +1,5 @@
 import axios from '../config/requestConfig.js';
+import{ picUrl, } from '@/api/common.js'
 
 export const postEvaluation = async (data) => {
 	console.log("postEvaluation");
@@ -8,5 +9,20 @@ export const postEvaluation = async (data) => {
 export const orderToEvaluation = async (data) => {
 	console.log("orderToEvaluation");
 	let result = await axios.get('order/'+data+'/rate');
+	return result;
+}
+export const idToEvaluation = async (data) => {
+	console.log("idToEvaluation");
+	let result = await axios.get('user/'+data+'/rate');
+	if(result && result.code){
+		console.log(result)
+	}else{
+		let x=0
+		let l = result.data.length;
+		for(x = 0;x<l;x++){
+			result.data[x].expert.expert_icon = picUrl+result.data[x].expert.expert_icon
+			result.data[x].enterprise.enterprise_icon = picUrl+result.data[x].enterprise.enterprise_icon
+		}
+	}
 	return result;
 }
--- pages/my-evaluations/my-evaluations.vue
+++ pages/my-evaluations/my-evaluations.vue
@@ -1,45 +1,70 @@
 <template>
-	<view class="my-evaluations" v-if='this.userInfo.type==5'>
+	<view class="my-evaluations" v-if='this.flag==1'>
 		<uni-section subTitle="显示你最近的评价" title="我的评价" type="line" >
-			<uni-card :is-shadow='false' title="123" extra="123" sub-title="123" :thumbnail='this.userInfo.userpic'>
+			<view v-if="this.userInfo.type==5">
+			<uni-card v-for='(eva,index) in rateList' :key="index" :is-shadow='false' :title="eva.expert.expert_name" :extra="eva.rate.datetime" :sub-title="eva.order.order_name" :thumbnail='eva.expert.expert_icon'>
 				<uni-collapse v-model="value">
 				<uni-collapse-item :show-animation="true" title="评价详情">
 				<view>
 				
 				
 				<uni-forms-item label="合作体验">
-					<uni-rate  value="4" :readonly="true" :is-fill="false"/>
+					<uni-rate  :value="eva.rate.rate_taste" :readonly="true" :is-fill="false"/>
 				</uni-forms-item>
 				<uni-forms-item label="完成速度">
-					<uni-rate  value="3" :readonly="true" :is-fill="false"/>
+					<uni-rate  :value="eva.rate.rate_speed" :readonly="true" :is-fill="false"/>
 				</uni-forms-item>
 				<uni-forms-item label="专业水准">
-					<uni-rate  value="5" :readonly="true" :is-fill="false"/>
+					<uni-rate  :value="eva.rate.rate_level" :readonly="true" :is-fill="false"/>
 				</uni-forms-item>
 				<uni-forms-item label="具体评价">
-					<text class='uni-body'>123</text>
+					<text class='uni-body' v-if='eva.rate.description'>{{eva.rate.description}}</text>
+					<text class='uni-body' v-else>无</text>
 				</uni-forms-item>
 				</view>
 				</uni-collapse-item>
 				</uni-collapse>
 			</uni-card>
+			</view>
 		</uni-section>
 		
+	</view>
+	<view class="mystyle u-f-ajc animated fadeIn" v-else>
+		<image src="/static/images/toast/no-data-03.jpg" 
+			mode="widthFix"></image>
+		<!-- <text>暂无数据</text> -->
 	</view>
 </template>
 
 <script>
 	import { mapState } from 'vuex'
-	
+	import {postEvaluation,orderToEvaluation,idToEvaluation} from '@/api/postEvaluation.js'
+	import{ picUrl, } from '@/api/common.js'
 	export default {
+		async onLoad(){
+			this.init()
+		},
 		computed: { ...mapState(['userInfo']) },
 		data() {
 			return {
 				value:true,
+				rateList:[],
+				flag:-1,
 			}
 		},
 		methods: {
-			
+			async init(){
+				let result = await idToEvaluation(this.userInfo.id)
+				if(result && result.code){
+					
+				}else{
+					
+					this.flag = result.flag
+					this.rateList = result.data
+					console.log(result.flag)
+					console.log(this.rateList[0].rate.rate_taste)
+				}
+			},
 		}
 	}
 </script>
