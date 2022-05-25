6e8bc038a6dd1ae6ed9de5f90e2fbfb616c7832f
cpfy <309301477@qq.com>
2022-05-25 17:24:15 +0800

    feat: 企业推荐专家入口醒目++

--- api/manage-need.js
+++ api/manage-need.js
@@ -1,33 +1,27 @@
-import axios from '@/config/requestConfig.js';
+import axios from '@/config/requestConfig.js'
 
-export const  manageUnfinishedNeed =async (id) => {
-	console.log("id is:" + id)
-	console.log("manageUnFinishedNeed")
-	let headers = {
-		"Authorization":'Bearer ' + uni.getStorageSync('token')
-	}
-	let result = await axios.get('user/' + id + "/need/proceeding", {}, headers)
+export const manageUnfinishedNeed =async id => {
+	console.log('id is:' + id)
+	console.log('manageUnFinishedNeed')
+	let headers = { 'Authorization':'Bearer ' + uni.getStorageSync('token') }
+	let result = await axios.get('user/' + id + '/need/proceeding', {}, headers)
 	result = result.data
 	return result
 }
 
-export const  manageFinishedNeed =async (id) => {
-	console.log("manageFinishedNeed")
-	let headers = {
-		"Authorization":'Bearer ' + uni.getStorageSync('token')
-	}
+export const manageFinishedNeed =async id => {
+	console.log('manageFinishedNeed')
+	let headers = { 'Authorization':'Bearer ' + uni.getStorageSync('token') }
 	
-	let result = await axios.get('user/' + id + "/need/finished", {}, headers)
+	let result = await axios.get('user/' + id + '/need/finished', {}, headers)
 	result = result.data
 	return result
 }
 
-export const  manageUnissuedNeed =async (id) => {
-	console.log("manageUnissuedNeed")
-	let headers = {
-		"Authorization":'Bearer ' + uni.getStorageSync('token')
-	}
-	let result = await axios.get('user/' + id + "/need/saved", {}, headers)
+export const manageUnissuedNeed =async id => {
+	console.log('manageUnissuedNeed')
+	let headers = { 'Authorization':'Bearer ' + uni.getStorageSync('token') }
+	let result = await axios.get('user/' + id + '/need/saved', {}, headers)
 	console.log(result)
 	result = result.data
 	// console.log("saved data should be:" + result)
@@ -35,42 +29,35 @@ export const  manageUnissuedNeed =async (id) => {
 }
 
 export const deleteNeed =async (company_id, id) => {
-	console.log("deleteNeed")
-	let headers = {
-		"Authorization":'Bearer ' + uni.getStorageSync('token')
-	}
-	let result = await axios.delete('user/' + company_id + "/need/" + id, {}, headers)
+	console.log('deleteNeed')
+	let headers = { 'Authorization':'Bearer ' + uni.getStorageSync('token') }
+	let result = await axios.delete('user/' + company_id + '/need/' + id, {}, headers)
 	return result
 }
 
 export const endNeed =async (company_id, id) => {
-	console.log("endNeed")
-	let headers = {
-		"Authorization":'Bearer ' + uni.getStorageSync('token')
-	}
-	let result = await axios.post('user/' + company_id + "/need/" + id + "/finish", {}, headers)
+	console.log('endNeed')
+	let headers = { 'Authorization':'Bearer ' + uni.getStorageSync('token') }
+	let result = await axios.post('user/' + company_id + '/need/' + id + '/finish', {}, headers)
 	return result
 }
 
-export const expertRecommend =async (id) => {
-	console.log("expertRecommend")
-	let headers = {
-		"Authorization":'Bearer ' + uni.getStorageSync('token')
-	}
+// 推荐专家
+export const expertRecommend =async id => {
+	console.log('expertRecommend')
+	let headers = { 'Authorization':'Bearer ' + uni.getStorageSync('token') }
 	let result = await axios.get('need/' + id + '/expert_recommend', {}, headers)
 	if (result && result.code) {
-		console.log("error!")
+		console.log('error!')
 	}
-	console.log("result is:" + result)
-	console.log("result is:" + result.data.length)
+	console.log('result is:' + result)
+	console.log('result is:' + result.data.length)
 	return result
 }
 
 export const transformNeed =async (uid, id) => {
-	console.log("transformNeed")
-	let headers = {
-		"Authorization":'Bearer ' + uni.getStorageSync('token')
-	}
+	console.log('transformNeed')
+	let headers = { 'Authorization':'Bearer ' + uni.getStorageSync('token') }
 	let result = await axios.post('user/'+ uid + '/need/' + id + '/transform', {}, headers)
 	return result
 }
